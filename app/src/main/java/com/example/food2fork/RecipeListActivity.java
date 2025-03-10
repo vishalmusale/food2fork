ebbfbc64bb68b1be4e0887ade862c029f9ea3be3
vishalmusale <vishal.m.musale@gmail.com>
2025-03-10 09:38:30 -0700

    Added Base Activity

--- app/src/main/java/com/example/food2fork/RecipeListActivity.java
+++ app/src/main/java/com/example/food2fork/RecipeListActivity.java
@@ -3,18 +3,17 @@ package com.example.food2fork;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RecipeListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
