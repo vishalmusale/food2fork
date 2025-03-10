2f82d38d75a508e6e4d41f60935476b5c540ca8f
vishalmusale <vishal.m.musale@gmail.com>
2025-03-10 10:32:43 -0700

    Retrofit response objects

---
+++ app/src/main/java/com/example/food2fork/requests/response/RecipeResponse.java
@@ -0,0 +1,19 @@
+package com.example.food2fork.requests.response;
+
+import com.example.food2fork.models.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RecipeResponse {
+
+    @SerializedName("recipe")
+    @Expose()
+    private Recipe recipe;
+
+    @Override
+    public String toString() {
+        return "RecipeResponse{" +
+                "recipe=" + recipe +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/food2fork/requests/response/RecipeSearchResponse.java
@@ -0,0 +1,25 @@
+package com.example.food2fork.requests.response;
+
+import com.example.food2fork.models.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RecipeSearchResponse {
+    @SerializedName("count")
+    @Expose()
+    private int count;
+
+    @SerializedName("recipes")
+    @Expose()
+    private List<Recipe> recipes;
+
+    @Override
+    public String  toString() {
+        return "RecipeSearchResponse{" +
+                "count=" + count +
+                ", recipes=" + recipes +
+                '}';
+    }
+}
