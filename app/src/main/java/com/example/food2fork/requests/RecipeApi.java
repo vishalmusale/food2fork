e2ec4626d652643e7f6aa41a6ca58d9e62e0ee36
vishalmusale <vishal.m.musale@gmail.com>
2025-03-10 10:42:35 -0700

    Retrofit interface model

--- app/src/main/java/com/example/food2fork/requests/RecipeApi.java
+++ app/src/main/java/com/example/food2fork/requests/RecipeApi.java
@@ -1,4 +1,20 @@
 package com.example.food2fork.requests;
 
+import com.example.food2fork.requests.response.RecipeResponse;
+import com.example.food2fork.requests.response.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+    @GET("/search")
+    Call<RecipeSearchResponse> searchRecipe (
+            @Query("q") String query
+    );
+
+    @GET("/get")
+    Call<RecipeResponse> getRecipe (
+            @Query("rId") String query
+    );
 }
